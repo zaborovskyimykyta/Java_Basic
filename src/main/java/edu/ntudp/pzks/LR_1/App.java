d1f09bbdb18be46871040d10dd89561cbde557ff
hiccond <hiccond@gmail.com>
2025-03-25 14:58:12 +0200

    Виконані завдання з Базової джави

--- src/main/java/edu/ntudp/pzks/LR_1/App.java
+++ src/main/java/edu/ntudp/pzks/LR_1/App.java
@@ -1,10 +1,10 @@
 package edu.ntudp.pzks.LR_1;
 
 /**
- * Hello world!
+ * Hello  world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         System.out.println("Hello World!");
     }
 }
