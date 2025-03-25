caa863dc07ac820b47ab0cf3a5a9eb9ba226c944
hiccond <hiccond@gmail.com>
2025-03-25 15:05:35 +0200

    Виконані завдання з Базової джави

--- src/main/java/edu/ntudp/pzks/LR_4/model/Head.java
+++
@@ -1,8 +0,0 @@
-package edu.ntudp.pzks.LR_4.model;
-
-
-public class Head extends Human {
-    public Head(String firstName, String lastName, String middleName, Sex sex) {
-        super(firstName, lastName, middleName, sex);
-    }
-}
